4412f21f3fe3c1c21e5d97dbe59679e3aa00fb13
MSebas55 <calimandrucsebas@gmail.com>
2023-11-10 12:15:41 +0100

    probando material design

---
+++ app/src/main/java/com/MSebas55/layoutinicio/MainActivity.java
@@ -0,0 +1,15 @@
+package com.MSebas55.layoutinicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
